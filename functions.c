cb3de5b82654a5f1fa7a86bb10a6c4f63470672e
obakeng <obakengmakgale13@gmail.com>
2023-04-25 05:48:46 -0700

    Updates

--- functions.c
+++ functions.c
@@ -9,6 +9,7 @@
  * @flags: calculates active flags
  * @width: width
  * @size: size specifier
+ * @precision: precision
  * Return: number of chars printed
  */
 
@@ -17,7 +18,8 @@ int print_char(va_list types, char buffer[],
 
 {
 	char c = va_arg(types, int);
-	return(handle_write_char(c, buffer, flags, width, precision, size));
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
 /**** print a string ****/
@@ -29,6 +31,7 @@ int print_char(va_list types, char buffer[],
  * @flags: calculates active flags
  * @width: width
  * @size: size specifier
+ * @precision: precision
  * Return: number of chars printed
  */
 
@@ -95,10 +98,10 @@ int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 
 {
-       	UNUSED(buffer);
-       	UNUSED(flags);
-       	UNUSED(width);
-       	UNUSED(precision);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
 	UNUSED(size);
 	UNUSED(types);
 	return (write(1, "%%", 1));
@@ -142,3 +145,54 @@ int print_int(va_list types. char buffer[],
 	i++;
 
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
+/**** PRINT BINARY ****/
+
+/**
+ * print_binary - prints unsigned number
+ * @types: list of arguments
+ * @buffer: buffer array that handles print
+ * @flags: claculates active flags
+ * @width: width
+ * @precision: precision
+ * @size: size specifier
+ * Return: number of chars printed
+ */
+
+int print_binary(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+unsigned int n, m, i, sum;
+unsigned int a[32];
+int count;
+
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+
+n = va_arg(types, unsigned int);
+m = 2147483648; /* (2 ^ 31)*/
+a[0] = n / m;
+for (i = 1; i < 32; i++)
+
+{
+	m /= 2;
+	a[i] = (n / m) % 2;
+}
+
+for (i = 0, sum = 0, count = 0; i < 32; i++)
+{
+	sum += a[i];
+	if (sum || i == 31)
+	{
+		char z = '0' + a[i];
+
+		write(1, &z, 1);
+		count++;
+	}
+}
+return (count);
+}
