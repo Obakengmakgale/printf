4ad304e78c86991b665d054a4726a1560ad592d8
obakeng <obakengmakgale13@gmail.com>
2023-04-25 02:39:13 -0700

    Updates

--- main.h
+++ main.h
@@ -41,5 +41,51 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[],
+		int flags, int width, int precision, int size);
 
+/********* FUNCTIONS *******/
+
+/* functions to print char and strings */
+
+int print_char(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+
+/* functions to print numbers */
+
+int print_int(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[],
+		char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
+/* function to print non printable characters */
+
+int print_non_printable(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+
+/* function to print memory address */
+int print_pointer(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+
+/* function to handle other specifiers */
+
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+#endif
